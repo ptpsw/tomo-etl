662eed0c088767eb2880d2f86f8bd8f08339b986
Robby Arifandri <robby.arifandri@gmail.com>
2021-09-14 21:29:35 +0700

    Add USL notebook and sdr model

--- sql_queries.py
+++ sql_queries.py
@@ -2,6 +2,7 @@
 station_table_drop = "DROP TABLE IF EXISTS stations;"
 station_link_table_drop = "DROP TABLE IF EXISTS station_links;"
 max_snr_table_drop = "DROP TABLE IF EXISTS max_snr;"
+sdr_table_drop = "DROP TABLE IF EXISTS sdr;"
 temp_table_drop = "DROP TABLE IF EXISTS temperature;"
 current_table_drop = "DROP TABLE IF EXISTS current;"
 
@@ -28,6 +29,15 @@ CREATE TABLE IF NOT EXISTS station_links (
 """
 max_snr_table_create = """
 CREATE TABLE max_snr (
+    station_id VARCHAR(10) NOT NULL, 
+    timestamp TIMESTAMP NOT NULL,
+    value FLOAT,
+    PRIMARY KEY(station_id, timestamp),
+    FOREIGN KEY (station_id) REFERENCES stations(station_id) ON DELETE CASCADE
+); 
+"""
+sdr_table_create = """
+CREATE TABLE sdr (
     station_id VARCHAR(10) NOT NULL, 
     timestamp TIMESTAMP NOT NULL,
     value FLOAT,
@@ -78,6 +88,12 @@ VALUES (%s, %s, %s)
 ON DUPLICATE KEY UPDATE value=VALUES(value);
 """
 
+sdr_table_insert = """
+INSERT INTO sdr (station_id, timestamp, value)
+VALUES (%s, %s, %s)
+ON DUPLICATE KEY UPDATE value=VALUES(value);
+"""
+
 temp_table_insert = """
 INSERT INTO temperature (station_link_id, timestamp, value)
 VALUES (%s, %s, %s)
@@ -111,12 +127,12 @@ WHERE
 create_table_queries = [
     station_table_create,
     station_link_table_create, 
-    max_snr_table_create, temp_table_create, current_table_create
+    max_snr_table_create, sdr_table_create, temp_table_create, current_table_create
 ]
 
 # Drop tables with tables with foreign key first
 drop_table_queries = [
-    max_snr_table_drop, temp_table_drop, current_table_drop,
+    max_snr_table_drop, sdr_table_drop, temp_table_drop, current_table_drop,
     station_link_table_drop, 
     station_table_drop
 ]
